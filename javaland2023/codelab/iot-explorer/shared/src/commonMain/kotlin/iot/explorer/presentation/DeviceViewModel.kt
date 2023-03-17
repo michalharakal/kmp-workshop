6ed7a4e00e331635a8db350f7d2d470540151058
Michal Harakal <michal.harakal@googlemail.com>
2023-03-17 20:57:25 +0100

    Add DeviceViewModel

---
+++ javaland2023/codelab/iot-explorer/shared/src/commonMain/kotlin/iot/explorer/presentation/DeviceViewModel.kt
@@ -0,0 +1,35 @@
+package iot.explorer.presentation
+
+import iot.explorer.data.PlcDeviceRepository
+import iot.explorer.domain.Device
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+data class DevicePresentationModel(val label: String = "", val temperature: String = "")
+
+class DeviceViewModel : CommonViewModel() {
+
+    private val repository = PlcDeviceRepository()
+
+
+    private val _state = MutableStateFlow(DevicePresentationModel())
+    val state: StateFlow<DevicePresentationModel>
+        get() = _state
+
+    init {
+        clientScope.launch {
+            repository.temperature.collect { temperature ->
+                _state.value = mapToViewData(temperature)
+            }
+        }
+    }
+
+    private fun mapToViewData(homePresenter: Device): DevicePresentationModel =
+        DevicePresentationModel(
+            label = homePresenter.name,
+            temperature = temperatureFormatter(homePresenter.temperature)
+        )
+
+    private fun temperatureFormatter(temperature: Float): String = "${temperature}°C"
+}
