7ecc22390090b205b0ce4b09c48aaf9d42c2fb78
Alexander von Below <alex@vonbelow.com>
2023-03-20 20:03:52 +0100

    Added CocoaMQTT as a direct dependency

--- javaland2023/codelab/iot-explorer/iOSExplorer/Pods/Target Support Files/Pods-iOSExplorer/Pods-iOSExplorer-frameworks.sh
+++ javaland2023/codelab/iot-explorer/iOSExplorer/Pods/Target Support Files/Pods-iOSExplorer/Pods-iOSExplorer-frameworks.sh
@@ -176,9 +176,13 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaMQTT/CocoaMQTT.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MqttCocoaAsyncSocket/MqttCocoaAsyncSocket.framework"
   install_framework "${PODS_ROOT}/../../shared/build/cocoapods/framework/shared.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaMQTT/CocoaMQTT.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MqttCocoaAsyncSocket/MqttCocoaAsyncSocket.framework"
   install_framework "${PODS_ROOT}/../../shared/build/cocoapods/framework/shared.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
