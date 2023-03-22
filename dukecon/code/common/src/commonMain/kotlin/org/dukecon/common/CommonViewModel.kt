36f38ae787145ed4101dcbd5a563099751d2676a
Michal Harakal <michal.harakal@googlemail.com>
2023-03-22 06:47:00 +0100

    remove dukecon from javaland branch and update readme

--- dukecon/code/common/src/commonMain/kotlin/org/dukecon/common/CommonViewModel.kt
+++
@@ -1,9 +0,0 @@
-package org.dukecon.common
-
-
-import kotlinx.coroutines.CoroutineScope
-
-expect open class CommonViewModel() {
-    val clientScope: CoroutineScope
-    protected open fun onCleared()
-}
--- dukecon/code/web/src/jsMain/kotlin/org/dukecon/web/ui/configuration/JavalandConfiguration.kt
+++
@@ -1,2 +0,0 @@
-package org.dukecon.web.ui.configuration
-
